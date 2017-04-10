91e72d021b56b3e2c28dbcc55b81c0fa74226de8
Suthicha Poonakaow <suthicha@outlook.com>
2017-04-10 16:03:35 +0700

    test menu event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app works!';
+
+
+  showmenu(){
+ 
+    let body = document.getElementsByTagName('body')[0]
+
+    if (body.classList.contains('hide-sidedrawer'))
+    {
+      body.classList.remove("hide-sidedrawer");
+    }else{
+      body.classList.add("hide-sidedrawer");      
+    }
+    
+  }
+
+  showSubmenu(event){
+    var target = event.target || event.srcElement || event.currentTarget;
+    var ul = target.nextElementSibling;
+
+    if (ul.style.display == 'block')
+    {
+      ul.style.display = 'none';
+    }else
+    {
+      ul.style.display = 'block';
+    }
+    
+  }
+
 }
